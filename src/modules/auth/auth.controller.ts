1bbb0d293f1f818a7b20fbb0b3733c043a93cf62
nnee-its <nam.dao@icetea.io>
2024-03-20 12:04:19 +0700

    feat: siwe

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,16 +1,35 @@
-import { Body, Controller, Post, UnauthorizedException } from "@nestjs/common"
-import { ApiTags } from "@nestjs/swagger"
-import { SiweMessage } from "siwe"
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UnauthorizedException,
+} from "@nestjs/common"
+import { ApiOkResponse, ApiTags } from "@nestjs/swagger"
+import { SiweMessage, generateNonce } from "siwe"
 import { SignInDto } from "./dto/sign-in.dto"
 
 @ApiTags("Auth")
 @Controller("auth")
 export class AuthController {
+  @ApiOkResponse({
+    schema: {
+      example: "QWkDDa9Kf6igwGBms",
+    },
+  })
+  @Get("nonce")
+  getNonce() {
+    return generateNonce()
+  }
+
   @Post("sign-in")
   async signIn(@Body() data: SignInDto) {
     const siweMessage = new SiweMessage(data.message)
-    const siweResponse = await siweMessage.verify({ signature: data.signature })
-    if (siweResponse.success) {
+    const siweResponse = await siweMessage.verify({
+      signature: data.signature,
+    })
+    if (siweResponse.data) {
+      console.log(siweResponse.data)
     } else throw new UnauthorizedException()
   }
 }
--- src/modules/auth/dto/sign-in.dto.ts
+++ src/modules/auth/dto/sign-in.dto.ts
@@ -1,9 +1,6 @@
-import { IsEthereumAddress, IsString } from "class-validator"
+import { IsString } from "class-validator"
 
 export class SignInDto {
-  @IsEthereumAddress()
-  walletAddress: string
-
   @IsString()
   signature: string
 
